ff81d9aef85708927b2d575d7113f9f6d5723a77
Bhatt <rajat-kumar.bhatt@capgemini.com>
2025-03-20 12:15:07 +0530

    test

--- 01_basics/testme.js
+++ 01_basics/testme.js
@@ -1,1 +1,15 @@
-console.log("Test me")
+console.log("Test")
+console.log(3+3)
+let name ="Rajat"
+let age = "30"
+let  isLoggedin = true
+
+//number => 2 to power 53
+//Bigint use for larg amount of value
+//string ="" which always use in ""
+//boolean = true/False
+//null =>Standalone Value 
+//Undefined =>
+    //symbol  =>unique
+//Object 
+console.log(typeof undefined)
