5d13eb9d5645086320d8d11146d11272a29f8751
Bhatt <rajat-kumar.bhatt@capgemini.com>
2025-03-20 15:01:17 +0530

    test

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,25 @@
-console.log("Rajat")
+//let score = "Rajat"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+//let valueinNumber = Number(score)
+//console.log(typeof valueinNumber);
+//console.log(valueinNumber);
+
+//"33">= 33
+// "33abc" => NaN not a number
+//true >= 1; false =>0;
+
+//let isLoggedin = 
+
+//let booleaIsloggedIn =Boolean(isLoggedin)
+//console.log(booleaIsloggedIn);
+
+// 1 => True; 0 => false
+//""=> false
+//"Rajat" =>true 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
